a860f582f58eed321ba2b2a6e042bea90fde9b8a
Srishti Raut <srishti.raut1102000@gmail.com>
2025-08-23 17:25:49 +0530

    feat(localStorage): Create an array of all events to be stored in the localStorage

--- scripts/event-store.js
+++ scripts/event-store.js
@@ -1,7 +1,10 @@
 export function initEventStore() {
     document.addEventListener("event-create", (event) => {
         const createdEvent = event.detail.event;
-        saveEventIntoLocalStorage([createdEvent]);
+        const events = getEventsFromLocalStorage();
+        events.push(createdEvent);
+
+        saveEventIntoLocalStorage(events);
 
     });
 }
@@ -12,14 +15,35 @@ function saveEventIntoLocalStorage(events) {
         ...event,
         date: event.date.toISOString()
     }));
-   
+
     let stringifiedEvents;
     try {
         stringifiedEvents = JSON.stringify(safeToStringifyEvents);
     } catch (error) {
         console.error("Failed to stringify events", error);
-        
+
     }
 
     localStorage.setItem("events", stringifiedEvents);
+}
+
+function getEventsFromLocalStorage() {
+    const localStorageEvents = localStorage.getItem("events");
+    if (localStorageEvents === null) {
+        return [];
+    }
+
+    let parsedEvents;
+    try {
+        parsedEvents = JSON.parse(localStorageEvents);
+    } catch (error) {
+        console.error("Failed to parse events", error);
+        return [];
+    }
+    const events = parsedEvents.map((event) => ({
+        ...event,
+        date: new Date(event.date)
+    }));
+
+    return events
 }
