a0f57a9b70f5076d6de7a4e7dc8595b70bac89aa
Srishti Raut <srishti.raut1102000@gmail.com>
2025-08-23 16:41:41 +0530

    styles(error-toast): add styling and animation to the red error toast that appears when event end time is before start time

--- scripts/toaster.js
+++ scripts/toaster.js
@@ -1,3 +1,5 @@
+import { waitUntilAnimationsFinish } from "./animation.js";
+
 export function initToaster(parent) {
     const toasterElement = document.createElement("div");
 
@@ -29,5 +31,24 @@ function createToast(message, type) {
 }
 
 function animateToast(toasterElement, toastElement) {
+    const heightBefore = toasterElement.offsetHeight;
     toasterElement.appendChild(toastElement);
+    const heightAfter = toasterElement.offsetHeight;
+    const heightDiff = heightAfter - heightBefore;
+
+    const toasterAnimation = toasterElement.animate([
+        { transform: `translate(0, ${heightDiff}px)` },
+        { transform: "translate(0,0)" }
+    ], {
+        duration: 150,
+        easing: "ease-out"
+    });
+
+    toasterAnimation.startTime = document.timeline.currentTime;
+
+    waitUntilAnimationsFinish(toastElement).then(() => {
+        toasterElement.removeChild(toastElement);
+    }).catch((error) => {
+        console.error("'Finish toast animation' promise failed", error);
+    });
 }
