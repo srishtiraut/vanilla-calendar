551453fae6d8fa7a919cb1a724735481bae1d8e6
Srishti Raut <srishti.raut1102000@gmail.com>
2025-08-24 16:35:50 +0530

    feat(date-nav): write helper functions for controlling and displaying dates

--- scripts/date.js
+++ scripts/date.js
@@ -8,3 +8,48 @@ export function today() {
         12
     );
 }
+
+export function addMonths(date, months){
+    const firstDayOfMonth = new Date(
+        date.getFullYear(),
+        date.getMonth() + months,
+        1,
+        date.getHours()
+    );
+    const lastDayOfMonth = getLastDayOfMonthDate(firstDayOfMonth);
+
+    const dayOfMonth = Math.min(date.getDate(), lastDayOfMonth.getDate());
+
+    return new Date(
+        date.getFullYear(),
+        date.getMonth() + months,
+        dayOfMonth,
+        date.getHours()
+    );
+}
+
+export function subtractMonths(date, months){
+    return addMonths(days, -months);
+}
+
+export function addDays(date, days){
+    return new Date(
+        date.getFullYear(),
+        date.getMonth(),
+        date.getDate() + days,
+        date.getHours()
+    );
+}
+
+export function subtractDays(date, days){
+    return addDays(date, -days);
+}
+
+function getLastDayOfMonthDate(date){
+    return new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0,
+        12
+    );
+}
--- scripts/nav.js
+++ scripts/nav.js
@@ -1,4 +1,4 @@
-import { today } from "./date.js";
+import { today, addDays, subtractDays, addMonths, subtractMonths } from "./date.js";
 
 const dateFormatter = new Intl.DateTimeFormat("en-US", {
     month: "long",
@@ -14,6 +14,35 @@ export function initNav() {
     let selectedView = "month";
     let selectedDate = today();
 
+    for (const todayButtonElement of todayButtonElements) {
+        todayButtonElement.addEventListener("click", () => {
+            todayButtonElement.dispatchEvent(new CustomEvent("date-change", {
+                detail: {
+                    date: today()
+                },
+                bubbles: true
+            }));
+        });
+    }
+
+    previousButtonElement.addEventListener("click", () => {
+        previousButtonElement.dispatchEvent(new CustomEvent("date-change", {
+            detail: {
+                date: getPreviousDate(selectedView, selectedDate)
+            },
+            bubbles: true
+        }));
+    });
+
+    nextButtonElement.addEventListener("click", () => {
+        nextButtonElement.dispatchEvent(new CustomEvent("date-change", {
+            detail: {
+                date: getNextDate(selectedView, selectedDate)
+            },
+            bubbles: true
+        }));
+    });
+
     refreshDateElement(dateElement, selectedDate);
 }
 
@@ -21,3 +50,28 @@ export function initNav() {
 function refreshDateElement(dateElement, selectedDate) {
     dateElement.textContent = dateFormatter.format(selectedDate);
 }
+
+function getPreviousDate(selectedView, selectedDate) {
+    if (selectedView === "day") {
+        return subtractDays(selectedDate, 1);
+    }
+
+    if (selectedView === "week") {
+        return subtractDays(selectedDate, 7);
+    }
+
+    return subtractMonths(selectedDate, 1);
+}
+
+function getNextDate(selectedView, selectedDate) {
+    if (selectedView === "day") {
+        return addDays(selectedDate, 1);
+    }
+
+    if (selectedView === "week") {
+        return addDays(selectedDate, 7);
+    }
+
+    return addMonths(selectedDate, 1);
+}
+
